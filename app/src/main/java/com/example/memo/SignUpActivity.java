3a420f2ea99e1f50d59f9d1d74c23ba2348cd29e
k-kyler <khaiquang690@gmail.com>
2022-03-27 11:57:27 +0700

    Fix sign up process

--- app/src/main/java/com/example/memo/SignUpActivity.java
+++ app/src/main/java/com/example/memo/SignUpActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     private TextInputEditText signUpEmail, signUpPassword, signUpConfirmPassword;
@@ -87,7 +89,7 @@ public class SignUpActivity extends AppCompatActivity {
                             finish();
                         } else {
                             progressBar.setVisibility(View.GONE);
-                            showToastMessage("Something went wrong");
+                            showToastMessage("This email have been used");
                         }
                     }
                 });
