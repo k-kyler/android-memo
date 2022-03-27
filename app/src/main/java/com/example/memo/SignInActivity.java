6d28305fc66d37bceb35bb63a31daacf03901992
Quang Khai <khaiquang690@gmail.com>
2022-03-27 15:24:06 +0700

    Feat/Sign-In (#6)
    * Add util method
    
    * Add sign in features

--- app/src/main/java/com/example/memo/SignInActivity.java
+++ app/src/main/java/com/example/memo/SignInActivity.java
@@ -1,14 +1,103 @@
 package com.example.memo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.memo.utils.ShowToastMessage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+    private TextInputEditText signInEmail, signInPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+
+    private final ShowToastMessage showToastMessage = new ShowToastMessage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        switchToSignUp();
+        signIn();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        checkIfSignedIn();
+    }
+
+    private void switchToSignUp() {
+        TextView switchToSignUpButton = findViewById(R.id.switchToSignUpButton);
+
+        switchToSignUpButton.setOnClickListener(view -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+
+            startActivity(intent);
+        });
+    }
+
+    private void signIn() {
+        signInEmail = findViewById(R.id.signInEmail);
+        signInPassword = findViewById(R.id.signInPassword);
+        progressBar = findViewById(R.id.progressBar);
+
+        Button signInButton = findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+
+            if (signInEmail.getText().toString().matches("")) {
+                showToastMessage.showToastMessage(getApplicationContext(), "Please enter your email");
+                progressBar.setVisibility(View.GONE);
+            } else if (signInPassword.getText().toString().matches("")) {
+                showToastMessage.showToastMessage(getApplicationContext(), "Please enter your password");
+                progressBar.setVisibility(View.GONE);
+            } else {
+                firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.signInWithEmailAndPassword(signInEmail.getText().toString(), signInPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+
+                            progressBar.setVisibility(View.GONE);
+                            showToastMessage.showToastMessage(getApplicationContext(), "Sign in successful");
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                            showToastMessage.showToastMessage(getApplicationContext(), "Wrong email or password");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void checkIfSignedIn() { ;
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/memo/SignUpActivity.java
+++ app/src/main/java/com/example/memo/SignUpActivity.java
@@ -1,24 +1,21 @@
 package com.example.memo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.memo.utils.ShowToastMessage;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     private TextInputEditText signUpEmail, signUpPassword, signUpConfirmPassword;
@@ -26,34 +23,25 @@ public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
 
+    private final ShowToastMessage showToastMessage = new ShowToastMessage();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        switchToLogin();
+        switchToSignIn();
         signUp();
     }
 
-    private void switchToLogin() {
-        TextView switchToLoginButton = findViewById(R.id.switchToSignInButton);
+    private void switchToSignIn() {
+        TextView switchToSignInButton = findViewById(R.id.switchToSignInButton);
 
-        switchToLoginButton.setOnClickListener(view -> {
+        switchToSignInButton.setOnClickListener(view -> {
             Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
 
             startActivity(intent);
         });
-    }
-
-    private void showToastMessage(String message) {
-        Context context = getApplicationContext();
-        int toastDuration = Toast.LENGTH_SHORT;
-
-        if (!message.matches("")) {
-            Toast toast = Toast.makeText(context, message, toastDuration);
-
-            toast.show();
-        }
     }
 
     private void signUp() {
@@ -68,13 +56,17 @@ public class SignUpActivity extends AppCompatActivity {
             progressBar.setVisibility(View.VISIBLE);
 
             if (signUpEmail.getText().toString().matches("")) {
-                showToastMessage("Please enter your email");
+                showToastMessage.showToastMessage(getApplicationContext(), "Please enter your email");
+                progressBar.setVisibility(View.GONE);
             } else if (signUpPassword.getText().toString().matches("")) {
-                showToastMessage("Please enter your password");
+                showToastMessage.showToastMessage(getApplicationContext(), "Please enter your password");
+                progressBar.setVisibility(View.GONE);
             } else if (signUpConfirmPassword.getText().toString().matches("")) {
-                showToastMessage("Please enter your confirm password");
+                showToastMessage.showToastMessage(getApplicationContext(), "Please enter your confirm password");
+                progressBar.setVisibility(View.GONE);
             } else if (!signUpPassword.getText().toString().equals(signUpConfirmPassword.getText().toString())) {
-                showToastMessage("Your passwords are not match");
+                showToastMessage.showToastMessage(getApplicationContext(), "Your passwords are not match");
+                progressBar.setVisibility(View.GONE);
             } else {
                 firebaseAuth = FirebaseAuth.getInstance();
                 firebaseAuth.createUserWithEmailAndPassword(signUpEmail.getText().toString(), signUpPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -84,12 +76,12 @@ public class SignUpActivity extends AppCompatActivity {
                             Intent intent = new Intent(SignUpActivity.this, ActiveAccountActivity.class);
 
                             progressBar.setVisibility(View.GONE);
-                            showToastMessage("Sign up successful");
+                            showToastMessage.showToastMessage(getApplicationContext(), "Sign up successful");
                             startActivity(intent);
                             finish();
                         } else {
                             progressBar.setVisibility(View.GONE);
-                            showToastMessage("This email have been used");
+                            showToastMessage.showToastMessage(getApplicationContext(), "This email have been used");
                         }
                     }
                 });
