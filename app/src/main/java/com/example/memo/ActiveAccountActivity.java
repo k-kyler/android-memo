615594329bea85bbb6c968102f9b6b6286f138af
k-kyler <khaiquang690@gmail.com>
2022-03-27 23:29:26 +0700

    Add activation features

--- app/src/main/java/com/example/memo/ActiveAccountActivity.java
+++ app/src/main/java/com/example/memo/ActiveAccountActivity.java
@@ -1,8 +1,14 @@
 package com.example.memo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActiveAccountActivity extends AppCompatActivity {
 
@@ -10,5 +16,48 @@ public class ActiveAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_account);
+
+        skipToMain();
+        sendVerificationEmail();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        reloadUser();
+    }
+
+    private void sendVerificationEmail() {
+        Button sendEmailButton = findViewById(R.id.sendEmail);
+
+        sendEmailButton.setOnClickListener(view -> {
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+            if (firebaseUser != null) {
+                firebaseUser.sendEmailVerification();
+                sendEmailButton.setEnabled(false);
+                sendEmailButton.setText("Sent");
+            }
+        });
+    }
+
+    private void skipToMain() {
+        TextView skipActivationButton = findViewById(R.id.skipActivation);
+
+        skipActivationButton.setOnClickListener(view -> {
+            Intent intent = new Intent(ActiveAccountActivity.this, MainActivity.class);
+
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void reloadUser() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            firebaseUser.reload();
+        }
     }
 }
