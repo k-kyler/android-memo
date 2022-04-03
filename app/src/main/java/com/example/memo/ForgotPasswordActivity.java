00220f6f052a71496cda40704980d7485f727135
k-kyler <khaiquang690@gmail.com>
2022-04-03 19:02:26 +0700

    Add forgot password handler

--- app/src/main/java/com/example/memo/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/memo/ForgotPasswordActivity.java
@@ -1,14 +1,40 @@
 package com.example.memo;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.memo.utils.ShowToastMessage;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPasswordActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+
+    private final ShowToastMessage showToastMessage = new ShowToastMessage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        sendRecoveryEmail();
+    }
+
+    private void sendRecoveryEmail() {
+        TextInputEditText recoveryEmail = findViewById(R.id.recoveryEmail);
+        Button sendRecoveryEmailButton = findViewById(R.id.sendPasswordResetEmailButton);
+
+        sendRecoveryEmailButton.setOnClickListener(view -> {
+            firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.sendPasswordResetEmail(recoveryEmail.getText().toString()).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    showToastMessage.showToastMessage(getApplicationContext(), "Email has been sent");
+                } else {
+                    showToastMessage.showToastMessage(getApplicationContext(), "Failed. Something went wrong");
+                }
+            });
+        });
     }
 }
